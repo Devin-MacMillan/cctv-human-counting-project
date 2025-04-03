30db9bfd27b5224cb5dd2ab0fae6f4da10d88b3a
Malia <mdhowe23@g.holycross.edu>
2025-04-03 18:02:27 -0400

    add Yolo file

---
+++ detYolo.py
@@ -0,0 +1,14 @@
+# YOLO detection
+'''
+Possible Improvements: Larger model size
+Mosaic (mix images to imporove context)
+Higher image resolution
+Include partially visible people in training
+Use negative samples in training
+'''
+class YoloDetecor:
+    # init code
+
+    # detection code
+
+    # code to draw box
